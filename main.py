f440f348484888983e1aa68867dfcacb7d717e3a
JaeHyeokSeong <seong5763@gmail.com>
2023-10-12 05:53:06 +1100

    solve conflict

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ def add(a, b):
 def multiply(a, b):
     return a * b
 
+def subtract(a, b):
+    return a - b
+
 if __name__ == '__main__':
     result = add(1, 2)
     print(f'result: {result}')
     result = multiply(2, 2)
+    print(f'result: {result}')
+    result = subtract(3, 1)
     print(f'result: {result}')
