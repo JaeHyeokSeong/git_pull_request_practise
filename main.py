52a7e5f990bdf2d23ccfb6d5b305054c4bdb4739
JaeHyeokSeong <107310053+JaeHyeokSeong@users.noreply.github.com>
2023-10-12 05:47:00 +1100

    create multiply method

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def add(a, b):
     return a + b
 
+def multiply(a, b):
+    return a * b
+
 if __name__ == '__main__':
     result = add(1, 2)
+    print(f'result: {result}')
+    result = multiply(2, 2)
     print(f'result: {result}')
